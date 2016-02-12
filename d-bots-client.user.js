0ec88dbe653c2ea5686ce8a71121e26a832fc308
d-bots-client <seppida@t-online.de>
2016-02-12 19:09:38 +0100

    Update d-bots-client.user.js

--- d-bots-client.user.js
+++ d-bots-client.user.js
@@ -22,7 +22,7 @@ var client_uuid = '3';   // ENTER HERE YOU'RE CLIENT-ID
 
 var logins = 0;        
 var bot_counter = 0;
-var socket = io.connect('ws://127.0.0.1:8081');
+var socket = io.connect('ws://127.0.0.1:8081'); // will change soon
 last_transmited_game_server = null;
 
    
